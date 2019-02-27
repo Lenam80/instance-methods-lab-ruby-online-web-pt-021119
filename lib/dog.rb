452ad5816005f521b72009a90df997aa76abfb9f
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-27 16:53:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+end
+
+  def initalize
+    bark
+  end
+
+def bark
+  puts "Woof"
+end
+
+def initalize
+  sit
+end
+
+def sit
+  puts "The Dog is Sitting"
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+end
+
+def initalize
+  talk
+  puts "Hello World"
+end
+
+def initalize
+  walk
+  puts "The Person is Walking"
+end
